308a080b9ca3a29c5fd13c07bc633c1e7644645b
haio <mockingror@gmail.com>
2014-12-19 18:44:23 +0800

    Fix names.get3

--- test/index.js
+++ test/index.js
@@ -1,6 +1,21 @@
-var randomName = require("../");
+var assert = require('assert');
+var randomName = require('..');
 
-for(var i = 0; i < 10000; i++) {
-    console.log(randomName.generate());
+function assertLength (name, length) {
+  var ok = !length ? ( name.length > 0 ) : name.length === length;
+  assert.ok(ok, 'Invlaid length!!' + name);
 }
 
+function run () {
+  assertLength(randomName.generate());
+  assertLength(randomName.surnames.getOne());
+  assertLength(randomName.names.get());
+  assertLength(randomName.names.get1(), 1);
+  assertLength(randomName.names.get2(), 2);
+  assertLength(randomName.names.get3(), 3);
+  assertLength(randomName.names.get1('火'), 1);
+  assertLength(randomName.names.get2('火火'), 2);
+  assertLength(randomName.names.get3('火火火'), 3);
+}
+
+for(var i = 0; i < 10000; i++) run();
