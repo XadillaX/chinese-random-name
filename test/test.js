017b0740d058ed0f01b2a7054c207a3940cb4a1a
Khaidi Chu <i@2333.moe>
2019-02-27 21:39:58 +0800

    fix: wrong length when generating (#5)

--- test/test.js
+++ test/test.js
@@ -4,11 +4,14 @@ var randomName = require("..");
 function assertLength (name, length) {
     console.log(name);
     var ok = !length ? (name.length > 0) : name.length === length;
-    assert.ok(ok, "Invlaid length!!" + name);
+    assert.ok(ok, "Invlaid length. " + name + " - " + length);
 }
 
 function run () {
     assertLength(randomName.generate());
+    assertLength(randomName.generate(2), 2);
+    assertLength(randomName.generate(3), 3);
+    assertLength(randomName.generate(4), 4);
     assertLength(randomName.surnames.getOne());
     assertLength(randomName.names.get());
     assertLength(randomName.names.get1(), 1);
